27c441ee1ae345bcca5a3e88eb26aa714cd12c04
kyh0113 <71127157+kyh0113@users.noreply.github.com>
2021-06-15 02:09:23 +0900

    Delete 백준 10816 (count).py

--- 백준 10816 (count).py
+++
@@ -1,8 +0,0 @@
-import sys
-N = int(sys.stdin.readline())
-array = list(map(int, sys.stdin.readline().split()))
-M = int(sys.stdin.readline())
-data = list(map(int, sys.stdin.readline().split()))
-
-for i in range(len(data)):
-    print(array.count(data[i]))
