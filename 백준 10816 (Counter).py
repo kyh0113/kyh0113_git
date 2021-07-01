f4c4abc126bb9117daf4261a804a8e6ce895491c
kyh0113 <71127157+kyh0113@users.noreply.github.com>
2021-07-01 17:43:11 +0900

    Update 백준 10816 (Counter).py

--- 백준 10816 (Counter).py
+++ 백준 10816 (Counter).py
@@ -7,6 +7,6 @@ M = int(sys.stdin.readline()) # 8
 data = list(map(int, sys.stdin.readline().split())) # 10 9 -5 2 3 4 5 -10
 array.sort() # -10 -10 2 3 3 6 7 10 10 10 
 
-array = Counter(array)
+array = Counter(array) 
 for i in data:
     print(array[i], end=" ")
