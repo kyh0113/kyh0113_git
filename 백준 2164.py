99633d4369444e34057c0116c2ebe3c964022ab1
kyh0113 <71127157+kyh0113@users.noreply.github.com>
2021-07-07 02:36:05 +0900

    Create 백준 2164.py

---
+++ 백준 2164.py
@@ -0,0 +1,12 @@
+import sys
+N= int(sys.stdin.readline())
+from collections import deque
+queue=deque()
+
+for i in range(1, N+1):
+    queue.append(i)
+
+while len(queue) > 1 :
+    queue.popleft()
+    queue.append(queue.popleft())
+print(queue.popleft())
